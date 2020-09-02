a6ae4a508eed961e3c5d1d6f473655b96daf88bb
tjjone98 <tzero1898@gmail.com>
2020-09-02 09:56:52 +0700

    feat:add car component

--- pokedesktop/src/page/Home/Home.tsx
+++ pokedesktop/src/page/Home/Home.tsx
@@ -1,14 +1,31 @@
+/**
+ *
+ * @author im6h
+ *
+ * Create at 30/8/2020.
+ * Update at 2/9/2020.
+ *
+ */
 import React from "react";
 import SearchBar from "../../component/Search/Search";
 import Category from "../../component/Category/Category";
 import Chip from "../../component/Chip/Chip";
-function Home(){
-  return(
+import Card from "../../component/Card/Card";
+
+function Home() {
+  return (
       <div>
-        <SearchBar />
-        <Chip name="fire" />
-        <Category name="pokedex" />
+        <SearchBar/>
+        <Chip name="fire"/>
+        <Category name="pokedex"/>
+        <Category name="items"/>
+        <Category name="type"/>
+        <Category name="location"/>
+        <Category name="abilities"/>
+        <Category name="moves"/>
+        <Card name="Pikachu" url={'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/002.png'}/>
       </div>
   )
 }
+
 export default Home;
