760cc38bb4ef9d78a1fe63afdf52c4d0800c2911
im6h <tzero1898@gmail.com>
2020-10-10 22:35:44 +0700

    error: reponsive

--- pokedesktop/src/component/Loading/Loading.tsx
+++ pokedesktop/src/component/Loading/Loading.tsx
@@ -1,19 +1,27 @@
+/**
+ *
+ * @author im6h
+ *
+ * Create at 2/9/2020.
+ * Update at 9/10/2020.
+ *
+ */
 import React from "react";
 import { Spin } from "antd";
+import styled from "styled-components";
 const Loading: React.FC<{}> = () => {
   return (
-    <div
-      style={{
-        width: "100%",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        flexDirection: "column",
-      }}
-    >
+    <Wrapper>
       <Spin size="large" />
-    </div>
+    </Wrapper>
   );
 };
+const Wrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
 
 export default Loading;
